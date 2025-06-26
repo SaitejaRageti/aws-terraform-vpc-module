1feaec1338e4b87d56ae2bcb1b346c10c82f5286
saitejarageti <saitejarageti129@gmail.com>
2025-06-26 08:19:08 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,4 +3,6 @@ module "aws_vpc" {
     project = "roboshop"
     environment = "dev"
     public_subnets_cidr = ["10.0.1.0/24","10.0.2.0/24"]
+    private_subnets_cidr = ["10.0.11.0/24","10.0.12.0/24"]
+    database_subnets_cidr = ["10.0.21.0/24","10.0.22.0/24"]
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
-output "az_names" {
-  value = module.aws_vpc.az_names
-}
+# output "az_names" {
+#   value = module.aws_vpc.az_names
+# }
+
+# output "public_subnet" {
+  
+#   value = module.aws_vpc.aws_public_subnet
+# }
